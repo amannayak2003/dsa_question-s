9a570b3d5fddaeef3c4ea416e71ff1e4272d9130
Aman Nayak <amannayak774@gmail.com>
2025-02-22 20:04:41 +0530

    finding Duplicate and Missing No'

--- Array/array_questions.java
+++ Array/array_questions.java
@@ -1,5 +1,8 @@
 package Array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class array_questions {
 
     public static void main(String[] args) {
@@ -20,5 +23,32 @@ public class array_questions {
         
         return candidate;
     }
+
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        
+        Set<Integer> set = new HashSet<>();
+        int[] result = new int[2];
+        int sum2 = 0;
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
+                sum2 += grid[i][j];
+                if(set.add(grid[i][j])==false){
+                    result[0] = grid[i][j];
+                }else{
+                    set.add(grid[i][j]);
+                }
+            }
+        }
+        int sum1 = 0 ;
+        for (int i = 1; i <= grid.length*grid[0].length; i++) {
+            sum1 += i;
+        }
+
+        result[1] = sum1 - (sum2-result[0]);
+
+        return result;
+
+    }
+    
     
 }
