316b9defd8fc6c2c56652fb0e1be3262260734f9
Aman Nayak <amannayak774@gmail.com>
2025-02-22 20:10:20 +0530

    Merge Sort without using extra space

--- Array/array_questions.java
+++ Array/array_questions.java
@@ -50,5 +50,21 @@ public class array_questions {
 
     }
     
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int c = m+n-1;
+        while(i>=0&&j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[c--] = nums1[i--]; 
+            }
+            else{
+                nums1[c--] = nums2[j--];
+            }
+        }
+        while(j>=0){
+            nums1[c--] = nums2[j--];
+        }
+    }
     
 }
