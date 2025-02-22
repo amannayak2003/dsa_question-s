6108dce2307aa2ce87f8b7f4be56e247909bb63d
Aman Nayak <amannayak774@gmail.com>
2025-02-23 00:04:59 +0530

    binary Exponent

--- Array/array_questions.java
+++ Array/array_questions.java
@@ -105,4 +105,23 @@ public class array_questions {
         
         return max;
     }
+
+    public double myPow(double x, int n) {
+        long binary = n;
+        double ans = 1;
+        if (n < 0) {
+            binary = -binary;
+            x = 1 / x;
+        }
+
+        while (binary > 0) {
+            if (binary % 2 == 1) {
+                ans *= x;
+            }
+            x = x * x;
+            binary /= 2;
+        }
+
+        return ans;
+    }
 }
